e48ae5294b03737ac46fc732e1c212a53552c304
RomanGuys <fat1x23@gmail.com>
2022-02-02 22:02:01 +0300

    add step?

--- src/test/java/CreateOrderTests.java
+++ src/test/java/CreateOrderTests.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -60,6 +61,7 @@ public class CreateOrderTests {
         assertEquals( true, response.path("success"));
     }
 
+   @Step("Логин")
     private String login(){
         String email = RandomStringUtils.randomAlphabetic(5) + "@yandex.ru";
         String password = RandomStringUtils.randomAlphabetic(10);
